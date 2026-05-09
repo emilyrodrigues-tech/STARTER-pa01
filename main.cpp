0db5f7edf82611eabc24d078ff5acdc1a53495fa
emilyrodrigues-tech <emilyrodrigues@ucsb.edu>
2026-05-08 22:08:55 -0700

    main.cpp

--- main.cpp
+++ main.cpp
@@ -8,33 +8,46 @@
 
 using namespace std;
 
-int main(int argv, char** argc){
-  if(argv < 3){
-    cout << "Please provide 2 file names" << endl;
-    return 1;
-  }
-  
-  ifstream cardFile1 (argc[1]);
-  ifstream cardFile2 (argc[2]);
-  string line;
+int main(int argc, char** argv) {
+    if (argc < 3) {
+        cout << "Please provide 2 file names" << endl;
+        return 1;
+    }
 
-  if (cardFile1.fail() || cardFile2.fail() ){
-    cout << "Could not open file " << argc[2];
-    return 1;
-  }
+    ifstream cardFile1(argv[1]);
+    ifstream cardFile2(argv[2]);
 
-  //Read each file
-  while (getline (cardFile1, line) && (line.length() > 0)){
+    if (cardFile1.fail() || cardFile2.fail()) {
+        cout << "Could not open file" << endl;
+        return 1;
+    }
 
-  }
-  cardFile1.close();
+    CardBST alice_hand;
+    CardBST bob_hand;
+    char suit;
+    string value;
 
+    while (cardFile1 >> suit >> value) {
+        alice_hand.insert(Card(suit, value));
+    }
+    cardFile1.close();
 
-  while (getline (cardFile2, line) && (line.length() > 0)){
+    while (cardFile2 >> suit >> value) {
+        bob_hand.insert(Card(suit, value));
+    }
+    cardFile2.close();
 
-  }
-  cardFile2.close();
-  
-  
-  return 0;
+    playGame(alice_hand, bob_hand);
+
+    cout << endl << "Alice's cards:" << endl;
+    for (auto it = alice_hand.begin(); it != alice_hand.end(); ++it) {
+        cout << *it << endl;
+    }
+
+    cout << endl << "Bob's cards:" << endl;
+    for (auto it = bob_hand.begin(); it != bob_hand.end(); ++it) {
+        cout << *it << endl;
+    }
+
+    return 0;
 }
