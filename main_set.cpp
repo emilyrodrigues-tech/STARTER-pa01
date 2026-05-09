830e4a7154cbe275dd693d018a92920baa40add4
emilyrodrigues-tech <emilyrodrigues@ucsb.edu>
2026-05-08 21:57:26 -0700

    main_set.cpp

--- main_set.cpp
+++ main_set.cpp
@@ -1,5 +1,3 @@
-// This file should implement the game using the std::set container class
-// Do not include card_list.h in this file
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -8,33 +6,69 @@
 
 using namespace std;
 
-int main(int argv, char** argc){
-  if(argv < 3){
-    cout << "Please provide 2 file names" << endl;
-    return 1;
-  }
-  
-  ifstream cardFile1 (argc[1]);
-  ifstream cardFile2 (argc[2]);
-  string line;
+int main(int argc, char** argv) { 
+        cout << "Please provide 2 file names" << endl;
+        return 1;
+    }
 
-  if (cardFile1.fail() || cardFile2.fail() ){
-    cout << "Could not open file " << argc[2];
-    return 1;
-  }
+    ifstream cardFile1(argv[1]);
+    ifstream cardFile2(argv[2]);
 
-  //Read each file
-  while (getline (cardFile1, line) && (line.length() > 0)){
+    if (cardFile1.fail() || cardFile2.fail()) {
+        cout << "Could not open file" << endl;
+        return 1;
+    }
 
-  }
-  cardFile1.close();
+    set<Card> alice_hand;
+    set<Card> bob_hand;
+    char suit;
+    string value;
 
+    while (cardFile1 >> suit >> value) {
+        alice_hand.insert(Card(suit, value));
+    }
+    cardFile1.close();
 
-  while (getline (cardFile2, line) && (line.length() > 0)){
+    while (cardFile2 >> suit >> value) {
+        bob_hand.insert(Card(suit, value));
+    }
+    cardFile2.close();
 
-  }
-  cardFile2.close();
-  
-  
-  return 0;
+    bool game_continues = true;
+    while (game_continues) {
+        game_continues = false;
+
+        for (auto it = alice_hand.begin(); it != alice_hand.end(); ++it) {
+            if (bob_hand.count(*it)) { 
+                cout << "Alice picked matching card " << *it << endl;
+                Card match = *it;
+                alice_hand.erase(match);
+                bob_hand.erase(match);
+                game_continues = true; 
+                break; 
+            }
+        }
+
+        if (!game_continues) break; 
+
+        game_continues = false;
+        for (auto it = bob_hand.rbegin(); it != bob_hand.rend(); ++it) {
+            if (alice_hand.count(*it)) {
+                cout << "Bob picked matching card " << *it << endl;
+                Card match = *it;
+                alice_hand.erase(match);
+                bob_hand.erase(match);
+                game_continues = true;
+                break; 
+            }
+        }
+    }
+
+    cout << endl << "Alice's cards:" << endl;
+    for (const auto& c : alice_hand) cout << c << endl;
+
+    cout << endl << "Bob's cards:" << endl;
+    for (const auto& c : bob_hand) cout << c << endl;
+
+    return 0;
 }
