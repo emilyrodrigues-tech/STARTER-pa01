6523c58d3d3bee7ef71113184685985beef985ff
emilyrodrigues-tech <emilyrodrigues@ucsb.edu>
2026-05-08 22:07:33 -0700

    card_list.cpp

--- card_list.cpp
+++ card_list.cpp
@@ -3,6 +3,8 @@
 // Implementation of the classes defined in card_list.h
 
 #include "card_list.h"
+#include <iostream>
+
 using namespace std;
 
 CardBST::CardBST() : root(nullptr) {}
@@ -43,6 +45,17 @@ bool CardBST::insert(const Card& card, Node *n) {
     }
 }
 
+bool CardBST::contains(const Card& card) const {
+    return getNodeFor(card, root) != nullptr;
+}
+
+CardBST::Node* CardBST::getNodeFor(const Card& card, Node* n) const {
+    if (!n) return nullptr;
+    if (card == n->info) return n;
+    if (card < n->info) return getNodeFor(card, n->left);
+    return getNodeFor(card, n->right);
+}
+
 
 CardBST::Iterator& CardBST::Iterator::operator++() {
     if (!curr) return *this;
@@ -76,7 +89,6 @@ CardBST::Iterator& CardBST::Iterator::operator--() {
     return *this;
 }
 
-
 CardBST::Iterator CardBST::begin() const {
     if (!root) return end();
     Node* n = root;
@@ -92,32 +104,58 @@ CardBST::Iterator CardBST::rbegin() const {
 }
 
 
-void playGame(CardBST& alice, CardBST& bob) {
-    bool foundMatch = true;
-    while (foundMatch) {
-        foundMatch = false;
+bool CardBST::remove(const Card& card) {
+    Node* n = getNodeFor(card, root);
+    if (!n) return false;
 
+    if (!n->left || !n->right) {
+        Node* child = (n->left) ? n->left : n->right;
+        if (n == root) {
+            root = child;
+            if (child) child->parent = nullptr;
+        } else {
+            if (n == n->parent->left) n->parent->left = child;
+            else n->parent->right = child;
+            if (child) child->parent = n->parent;
+        }
+        delete n;
+    } 
+    else {
+        Node* successor = n->right;
+        while (successor->left) successor = successor->left;
+        
+        Card temp = successor->info;
+        remove(temp); 
+        n->info = temp; 
+    }
+    return true;
+}
+
+
+void playGame(CardBST& alice, CardBST& bob) {
+    bool found = true;
+    while (found) {
+        found = false;
         for (auto it = alice.begin(); it != alice.end(); ++it) {
             if (bob.contains(*it)) {
+                cout << "Alice picked matching card " << *it << endl;
                 Card match = *it;
-                cout << "Alice picked matching card " << match << endl;
                 alice.remove(match);
                 bob.remove(match);
-                foundMatch = true;
+                found = true;
                 break;
             }
         }
+        if (!found) break;
 
-        if (!foundMatch) break;
-
-        foundMatch = false;
+        found = false;
         for (auto it = bob.rbegin(); it != bob.rend(); --it) {
             if (alice.contains(*it)) {
+                cout << "Bob picked matching card " << *it << endl;
                 Card match = *it;
-                cout << "Bob picked matching card " << match << endl;
                 alice.remove(match);
                 bob.remove(match);
-                foundMatch = true;
+                found = true;
                 break;
             }
         }
