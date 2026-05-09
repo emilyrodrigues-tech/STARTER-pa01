6ef9a8cc44f8ef03c37a5d560e4c47bf51d6c909
emilyrodrigues-tech <emilyrodrigues@ucsb.edu>
2026-05-08 21:50:48 -0700

    card.h

--- card.h
+++ card.h
@@ -1,8 +1,30 @@
 // card.h
-// Author: Your name
+// Author: Emily Rodirgues
 // All class declarations related to defining a single card go here
 
 #ifndef CARD_H
 #define CARD_H
 
+#include <iostream>
+#include <string>
+
+class Card {
+public: 
+  Card(char suit, std::string value);
+
+  bool operator<(const Card& other) const;
+  bool operator==(const Card& other) const;
+  bool operator>(const Card& other) const;
+
+  friend std::ostream& operator<<(std::ostream& os, const Card& card);
+
+private:
+  char suit;
+  std::string value;
+
+  int suiteWeight() const;
+  int valueWeight() const;
+
+};
+  
 #endif
